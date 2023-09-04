fadecf296d38a731bad4b4df70cd5c96b73b2e3c
trush1n <trushin.i.n@gmail.com>
2023-09-04 22:20:25 +0300

    Update 02.js

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,5 +1,5 @@
-function factorial(n)
-        debugger;{
+debugger;
+function factorial(n) {
     if (n === 0 || n === 1) {
         return 1;
     } else {
@@ -7,3 +7,4 @@ function factorial(n)
     }
 }
 console.log(factorial(5));
+
