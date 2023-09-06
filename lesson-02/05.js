d606dbcbb9c16aef04553edae8f30ada55da12f8
trush1n <trushin.i.n@gmail.com>
2023-09-06 21:53:01 +0300

    Update 05.js

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,7 +2,7 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
+console.log(passport);
 let passport1 = {...passport};
 passport1.name = "Ivan";
-console.log(passport);
 console.log(passport1);
