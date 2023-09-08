83b3efe28480334ebae9dc8ee480591f493c89eb
trush1n <trushin.i.n@gmail.com>
2023-09-08 18:02:03 +0300

    Update 06.js

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -10,3 +10,15 @@ let passportWithAddress2 = {...passportWithAddress,...name,...surname, address:{
 passportWithAddress2.address.city = 'Bobryisk';
 console.log(passportWithAddress1.address.city);
 console.log(passportWithAddress2.address.city);
+
+const kitchenSink = {
+  set: new Set([1, 3, 3]),
+  map: new Map([[1, 2]]),
+  regex: /foo/,
+  deep: { array: [ new File(someBlobData, 'file.txt') ] },
+  error: new Error('Hello!')
+}
+kitchenSink.circular = kitchenSink
+
+// ✅ Выполнено полное глубокое копирование
+const clonedSink = structuredClone(kitchenSink)
