c71fab9fe7033bfd3a7b43df905cc60c635e5d20
trush1n <trushin.i.n@gmail.com>
2023-09-11 22:59:19 +0300

    Update 07.js

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,7 +1,3 @@
-/* Ну и вишенка на торт: раз наш парень оказался «в нужное время в нужном месте», пора бы его женить):
-Под surname добавь married: true. И разумеется проверь, что женат
- только один парень… (Т.е. необходимо создать глубокую копию объекта passportMarried , и добавить в него новый ключ: значение).
-*/
 let passportMarried = {
     name: "Petr",
     surname: "Petrov",
@@ -10,8 +6,9 @@ let passportMarried = {
         city: "Bobryisk"
     }
 };
-let passportMarried2 = { ...pushmarried: true, address: { ... } } 
-passportMarried2.married = ...
 
-console.log(passportMarried);
+let passportMarried2 = { ...passportMarried, married: true, address: { ...passportMarried.address } }; 
+passportMarried2.married = true;
+ 
+console.log(passportMarried); 
 console.log(passportMarried2);
