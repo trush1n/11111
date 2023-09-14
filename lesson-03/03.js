3f490e743ad36e19e063414b0864a97f1b3258e5
trush1n <trushin.i.n@gmail.com>
2023-09-14 11:08:50 +0300

    Update 03.js

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,5 +1,3 @@
-​const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
-const fruits1 = fruits.slice(1, fruts.length -2);
-console.log(fruits1)
-
-
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+const fruits1 = fruits.slice(1, -2);
+console.log(fruits1);
